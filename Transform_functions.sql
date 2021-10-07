6241f7406bd2eb3e11322f05a8ac4ab519994514
an7user <90646142+an7user@users.noreply.github.com>
2021-10-07 10:33:24 -0700

    Add files via upload

---
+++ Transform_functions.sql
@@ -0,0 +1,75 @@
+-- CASE WHEN function
+
+SELECT *,
+case 
+when rating > 9.5 and unit_price < 30 then "Best product"
+ELSE 'other'
+END as product_select
+FROM fact_sales f
+join invoice i on f.invoice_id = i.invoice_id
+join customer c on f.customer_id = c.customer_id
+join location l on f.location_id = l.location_id
+order by product_select
+; 
+
+-- COALESCE function
+
+SELECT* from customer;
+
+SELECT* from customer
+WHERE membership is null;
+
+SELECT *,
+COALESCE (membership, 'NO MEMBERSHIP') as DA_MEMBA
+from customer;
+
+-- USING NULLIF function
+
+SELECT *,
+NULLIF (product_line, 'Sports and travel') AS NULLIFIED
+FROM fact_sales f
+join invoice i on f.invoice_id = i.invoice_id
+join customer c on f.customer_id = c.customer_id
+join location l on f.location_id = l.location_id
+; 
+
+--  LEAST and GREATEST
+
+SELECT *,
+GREATEST (50, unit_price) AS INFLATION_PRICE
+FROM fact_sales f
+join invoice i on f.invoice_id = i.invoice_id
+join customer c on f.customer_id = c.customer_id
+join location l on f.location_id = l.location_id
+; 
+
+SELECT *,
+LEAST (30, unit_price) AS HAPPY_PRICE
+FROM fact_sales f
+join invoice i on f.invoice_id = i.invoice_id
+join customer c on f.customer_id = c.customer_id
+join location l on f.location_id = l.location_id
+; 
+
+-- USING DISTINCT
+
+SELECT distinct unit_price
+FROM fact_sales;
+
+-- finding the same price
+
+SELECT *
+FROM fact_sales
+GROUP BY unit_price
+HAVING COUNT(unit_price) >1
+;
+
+-- CTE for same price
+
+WITH CTE as  
+(  
+   SELECT*, ROW_NUMBER() over (PARTITION BY unit_price ORDER BY unit_price) as same_price  
+   FROM fact_sales  
+)  
+SELECT * FROM CTE
+WHERE same_price > 1;
---
+++ dupe_del.sql
@@ -0,0 +1,49 @@
+-- / simple finding duplicates /
+
+SELECT 
+gross_income, 
+unit_price,
+COUNT(*) AS CNT
+FROM
+    fact_sales f
+    GROUP BY 
+    gross_income, 
+unit_price
+       HAVING COUNT(*) > 1;
+       
+     -- ? simple select with * for duplicates ?
+       
+       SELECT *, 
+COUNT(*) AS CNT
+FROM
+    fact_sales f
+    GROUP BY 
+    gross_income, gross_margin_percentage, quantity, unit_price
+       HAVING COUNT(*) > 1;
+       
+       -- / CTE finding duplicates /
+              
+WITH CTE as  
+(  
+   SELECT*, ROW_NUMBER() over (PARTITION BY invoice_id ORDER BY invoice_id) as Duplicates  
+   FROM fact_sales  
+)  
+SELECT * FROM CTE;
+
+-- WHERE Duplicates > 1;
+
+
+-- ROW_MUMBER function select
+   
+   SELECT * FROM (SELECT *, ROW_NUMBER() over (PARTITION BY invoice_id ORDER BY invoice_id) as Duplicates  
+   FROM fact_sales) as temp_table WHERE Duplicates >= 1;
+ 
+-- / Deliting duplicates Using the ROW_NUMBER() Function /
+
+ DELETE FROM fact_sales WHERE fact_id IN(
+   SELECT fact_id FROM (SELECT fact_id, ROW_NUMBER() 
+   over (PARTITION BY invoice_id ORDER BY invoice_id) as Duplicates  
+   FROM fact_sales) AS temp_table WHERE Duplicates >1
+   );
+    
+  select * from fact_sales;     
